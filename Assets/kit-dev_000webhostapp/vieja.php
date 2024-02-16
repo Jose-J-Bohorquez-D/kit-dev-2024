0e5a804f6be5762a2a858e312374e0ec56b17e19
Jose Bohorquez <bd567358546@gmail.com>
2024-02-16 05:18:49 -0500

    agregue el kit dev de 000webhost

---
+++ Assets/kit-dev_000webhostapp/vieja.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kit-Dev</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+  </head>
+  <body style="background-color: black;">
+
+<nav class="navbar bg-primary navbar-expand-lg" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Kit-Dev</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+        </li><!--
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Cuentas-Donaciones
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Veci</a></li>
+            <li><a class="dropdown-item" href="#">Dale</a></li>
+            <li><a class="dropdown-item" href="#">Movii</a></li>
+            <li><a class="dropdown-item" href="#">Nequi</a></li>
+            <li><a class="dropdown-item" href="#">Daviplata</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">3178773186</a></li>
+          </ul>
+        </li>--><!--
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>-->
+      <form class="d-flex" role="search"><!--
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>-->
+      </form>
+    </div>
+  </div>
+</nav>
+
+
+
+<div class=" text-center" style="color: white;">
+    <br>
+    <!-- <h1>Materiales de apoyo</h1>
+    <p>este material de apoyo lo suministra Jose</p>   -->
+    <p>aplicaciones impresindibles 
+    para tu formacion</p>
+</div>
+
+
+<br>
+
+<div class="container d-flex justify-content-evenly align-items-center text-center justify-content-center">
+
+  <div class="row">
+
+    <div class="col">
+
+      <div class="card" style="width: 18rem;">
+        <img src="imgs/ksweb.webp" class="img-thumbnail rounded float-start">
+        <div class="card-body">
+          <h5 class="card-title">KSWEB PRO</h5>
+          <p class="card-text">  </p>
+          <a href="https://drive.google.com/file/d/1-O3UI2kjw4F0--96qkn3A5r2lZccA68V/view?usp=share_link" target="_blank" class="btn btn-primary">Descargar</a>
+        </div>
+      </div><br><br>
+
+      <div class="card" style="width: 18rem;">
+        <img src="imgs/acode.webp" class="img-thumbnail rounded float-start">
+        <div class="card-body">
+          <h5 class="card-title">ACODE PRO</h5>
+          <p class="card-text">  </p>
+          <a href="https://drive.google.com/file/d/14t5oyKwfRgODGsxyjUMlARzQXfiH_5_Q/view?usp=share_link" target="_blank" class="btn btn-primary">Descargar</a>
+        </div>
+      </div><br><br>
+
+    </div><br><br>
+
+    <div class="col">
+
+      <div class="card" style="width: 18rem;">
+        <img src="imgs/esFileEx.webp" class="img-thumbnail rounded float-start">
+        <div class="card-body">
+          <h5 class="card-title">ES FILE EXPLORER PRO</h5>
+          <p class="card-text">  </p>
+          <a href="https://drive.google.com/file/d/1PJKAZcQ-Io3ZvVUHmYZF5pyrgo8De6Hs/view?usp=share_link" target="_blank" class="btn btn-primary">Descargar</a>
+        </div>
+      </div><br><br>
+
+      <div class="card" style="width: 18rem;">
+        <img src="imgs/termux.webp" class="img-thumbnail rounded float-start">
+        <div class="card-body">
+          <h5 class="card-title">TERMUX</h5>
+          <p class="card-text">  </p>
+          <a href="https://drive.google.com/file/d/1aAkifGbVQt1E2FRyTqa4XGBu_-s5IAl8/view?usp=share_link" target="_blank" class="btn btn-primary">Descargar</a>
+        </div>
+      </div><br><br>
+
+    </div><br><br>
+
+    <div class="col">
+
+      <div class="card" style="width: 18rem;">
+        <img src="imgs/wps.webp" class="img-thumbnail rounded float-start">
+        <div class="card-body">
+          <h5 class="card-title">WPS(office) PRO</h5>
+          <p class="card-text">  </p>
+          <a href="https://drive.google.com/file/d/1LqtqMIeqtsCOyOgxVUYVuhAvXWzmaQbd/view?usp=share_link" target="_blank" class="btn btn-primary">Descargar</a>
+        </div>
+      </div><br><br>
+
+    </div><br><br>
+
+  </div>
+
+</div>
+
+
+  <div class=" text-center" style="color: white;">
+    <!--<h1>Libros de apoyo</h1>
+    <p>este material de apoyo lo suministra Jose</p>  --> 
+    <p>libros esenciales para una buena apropiacion del conocimiento</p>
+</div><br><br>
+<section class="container d-flex justify-content-evenly  text-center ">
+
+  <div class="row d-flex justify-content-center">
+    <div class="col">
+
+      <div class="card" style="width: 18rem;">
+        <img src="imgs/pdf.webp" class="img-thumbnail rounded float-start" height="150px" width="150px">
+        <div class="card-body">
+          <h5 class="card-title">GIT pro (Libro PDF)</h5>
+          <p class="card-text">  </p>
+          <a href="libros/git.pdf" target="_blank" class="btn btn-primary">Descargar</a>
+        </div>
+      </div><br><br>
+
+      <div class="card" style="width: 18rem;">
+        <img src="imgs/pdf.webp" class="img-thumbnail rounded float-start" height="150px" width="150px">
+        <div class="card-body">
+          <h6 class="card-title">html,css y javascript (Libro PDF)</h6>
+          <p class="card-text">  </p>
+          <a href="libros/html,css,javascript.pdf" target="_blank" class="btn btn-primary">Descargar</a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+    
+
+
+
+
+<br><br><br><br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+  </body>
+</html>
